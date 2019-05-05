990167f4ec367ffaa8c16ed99a0d914b6c0534bd
PreethaNadipally <50114395+PreethaNadipally@users.noreply.github.com>
2019-05-05 21:28:39 +0530

    Update reversearray.c

--- reversearray.c
+++ reversearray.c
@@ -3,10 +3,11 @@ int main()
 {
 int n,c,d,a[100],b[100];
 scanf("%d",&n);
+for(c=0;c<n;c++)
 scanf("%d",&a[c]);
 for(c=n-1,d=0;c>=0;c--,d++)
 b[d]=a[c];
 for(c=0;c<n;c++)
-printf("%d",a[c]);
+printf("%d",b[c]);
 return 0;
 }
