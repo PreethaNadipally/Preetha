08257188f30ed2e94b4472bc7a50f814ee7fa042
PreethaNadipally <preetha.nadipally@gmail.com>
2019-05-05 19:55:24 +0530

    reverse of array of size n created

---
+++ reversearray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int n,c,d,a[100],b[100];
+scanf("%d",&n);
+scanf("%d",&a[c]);
+for(c=n-1,d=0;c>=0;c--,d++)
+b[d]=a[c];
+for(c=0;c<n;c++)
+printf("%d",a[c]);
+return 0;
+}
