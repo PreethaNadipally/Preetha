609073a4f720c132bb0deed3dae93406573591e6
PreethaNadipally <50114395+PreethaNadipally@users.noreply.github.com>
2019-05-10 18:51:49 +0530

    Update reversearray.c

--- reversearray.c
+++ reversearray.c
@@ -5,7 +5,7 @@ int n,index,arr1[100],temp;
 scanf("%d",&n);
 for(index=0;index<n;index++)
   scanf("%d",&arr1 [index]);
-for(index=0;index<n;index++)
+for(index=0;index<n/2;index++)
 {
   temp=arr1[index];
   arr1[index]=arr1[n-1-index];
