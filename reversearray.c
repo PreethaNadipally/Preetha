24cab2e5a954136ec977aa57c054321d50f596bc
PreethaNadipally <50114395+PreethaNadipally@users.noreply.github.com>
2019-05-10 18:07:03 +0530

    Update reversearray.c

--- reversearray.c
+++ reversearray.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 main()
 {
-int n,index,arr1[100];
+int n,index,arr1[100],temp;
 scanf("%d",&n);
 for(index=0;index<n;index++)
-  scanf("%d",&a[index]);
-for(index>n;index>0;index--)
-  printf("%d",arr1[index]);
+  scanf("%d",&arr1 [index]);
+for(index=0;index<n;index++)
+{
+  temp=arr1[index];
+  arr1[index]=arr1[n-1-index];
+  arr1[n-1-index]=temp;
+}
+   printf("%d",arr1[index]);
 }
